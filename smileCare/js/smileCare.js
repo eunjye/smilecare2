caaa9f1255e298a94a98cdb8d2740de5156d6c77
eunjye <eunjh1220@naver.com>
2020-11-05 11:22:32 +0900

    dddd

--- smileCare/js/smileCare.js
+++ smileCare/js/smileCare.js
@@ -322,16 +322,16 @@ var slidePage = {
 		if (!isMobile) {
 			_w = 720; _h = 350;
 		} else {
-			_w = $(window).outerWidth() - 50;
-			_h = $(window).outerHeight() - 230;
+			_w = $(window).outerWidth() - 50; _h = 350;
 		}
 		$panelWrap.css({width: _w*$panel.length}); // _w: apply-inner의 width
 		$panel.css({width: _w, height: _h}); // _h: apply-inner의 height
 
 		if (isMobile) {
 			$(window).on('resize', function(){
-				_h = $(window).outerHeight() - 230;
-				$panel.css({width: _w, height: _h});
+				if ($(window).outerHeight() < 580)
+					$panel.css('height', $(window).outerHeight() - 230);
+					console.log($(window).outerHeight() - 230);
 			})
 		}
 	},
